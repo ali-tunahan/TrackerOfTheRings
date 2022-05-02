ee951d8b7a8c29c40fdf99e958a92aacd7e7c24a
Deniz Unal <alidenizunal11@gmail.com>
2022-05-02 22:05:11 +0300

    javafiles

---
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -0,0 +1,49 @@
+public abstract class Account {
+    private String username;
+    private String password;
+    private String companyID;
+    private Company company;
+
+    //getters and setters
+    public String getUsername() {
+        return username;
+    }
+    public Company getCompany() {
+        return company;
+    }
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+    public String getCompanyID() {
+        return companyID;
+    }
+    public void setCompanyID(String companyID) {
+        this.companyID = companyID;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    //to do
+    public Company authenticateCompanyID(String ID){
+        return new Company();
+    }
+
+    public boolean login(String name, String password, String companyID){
+        //magical stuff from firebase
+
+        return true;
+    }
+
+
+
+
+    
+    
+}
---
+++ app/src/main/java/com/example/myapplication/Vehicle.java
@@ -0,0 +1,152 @@
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.time.*;
+
+public class Vehicle extends Account implements Locatable
+{
+    
+    private HashMap<Stop, String> history;
+    private boolean isActive;
+    private Route currentRoute;
+    private Location location;
+
+ 
+    /**
+     * Constructor 
+     * @param aName
+     * @param aPassword
+     * @param aCompanyID
+     */
+    public Vehicle(String aName, String aPassword, String aCompanyID){
+        this.setUsername(aName);
+        this.setPassword(aPassword);
+        this.setCompanyID(aCompanyID);
+        this.setCompany(authenticateCompanyID(aCompanyID));
+    }
+
+/**
+ * Empty constructor
+ */
+    public Vehicle(){}
+    
+    /**
+	 * Accessor method for boolean isActive
+	 * @return isActive
+	 */
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    /**
+	 * Mutator for boolean isActive
+	 * @param isActive
+	 */
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+
+    /**
+     * If the vehicle is near 20 meters of a stop method returns true using the haversine formula
+     * @return boolean arrivedAtStop
+     * https://www.movable-type.co.uk/scripts/latlong.html
+     */
+    public boolean arrivedAtStop(){
+
+        double R = 6371000;
+        double number1 = this.getLocation().getLatitude()*Math.PI/180;
+        double number2 = this.getNextStop().getLocation().getLatitude()*Math.PI/180;
+        double delta1 = (this.getNextStop().getLocation().getLatitude()-this.getLocation().getLatitude())*Math.PI/180;
+        double delta2 = (this.getNextStop().getLocation().getLongitude()-this.getLocation().getLongitude())*Math.PI/180;
+        double a= Math.sin(delta1/2)* Math.sin(delta1/2) + Math.cos(number1)*Math.cos(number2)*Math.sin(delta2/2)*Math.sin(delta2/2);
+        double result = Math.atan2(Math.sqrt(a), Math.sqrt(1-a))*2*R;
+
+        if( result < 20){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Adds the time to history when the vehicle passes a stop and updates the currentRoute
+     */
+    public void addToHistory(){
+        if(this.arrivedAtStop()){
+            Clock clock = Clock.systemDefaultZone();
+            Instant instant = clock.instant();
+            this.history.put(this.getNextStop(), instant.toString());
+            
+            Route newRoute = new Route();
+            newRoute.setName = currentRoute.getName;
+            ArrayList<Stop> copyStops = new ArrayList<Stop>();
+
+            for(int i = 1; i < currenRoute.getStopsList.size();i++){ // i = 1 so that the current route does not involve passed stop
+                copyStops.add(currenRoute.getStopsList.get());
+            }
+            this.setCurrentRoute(newRoute);
+            
+
+        }
+    }
+
+    /**
+	 * İnitializes the copy of the chosen route to currentRoute
+	 * @param aCurrentRoute
+	 */
+    public void setCurrentRoute(Route aCurrenRoute){
+        Route copy = new Route();
+        copy.setName = aCurrenRoute.getName;
+
+        ArrayList<Stop> copyStops = new ArrayList<Stop>();
+
+        for(int i = 0; i < aCurrenRoute.getStopsList.size();i++){
+            copyStops.add(aCurrenRoute.getStopsList.get(i));
+        }
+        this.currentRoute = copy;
+    }
+
+    /**
+	 * Accessor method for currentRoute
+	 * @return currentRoute
+	 */
+    public Route getCurrentRoute(){
+        return this.currentRoute;
+    }
+
+    /**
+     * Accessor method for location
+     * @return location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Mutator for location
+     * @param aLocation
+     */
+    public void setLocation(Location aLocation) {
+        this.location = aLocation;
+    }
+    /**
+     * Accessor for nextStop
+     * @return nextStop
+     */
+    public Stop getNextStop(){
+       return this.currentRoute.getStopsList.get(0);
+    }
+
+    /**
+     * Accessor for vehicle history
+     * @return history
+     */
+    public HashMap<Stop, String> getHistory(){
+        return history;
+    }
+
+
+
+}
