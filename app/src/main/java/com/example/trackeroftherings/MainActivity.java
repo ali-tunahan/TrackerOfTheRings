7574e4389b5bf96234242eb02687c7fe7ca81b12
Pesichra <eniskeremcakmak@gmail.com>
2022-05-03 14:41:47 +0300

    Fix error for toolbar

--- app/src/main/java/com/example/trackeroftherings/MainActivity.java
+++ app/src/main/java/com/example/trackeroftherings/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        //setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
