41ce933402573a2a3ed88cab9b5129281889860c
Ali Tunahan Işık <tunahan0735@gmail.com>
2022-05-04 15:22:15 +0300

    Finished the companyLogin method. The LoginUtility class is complete.

--- app/src/main/java/com/example/trackeroftherings/LoginUtility.java
+++ app/src/main/java/com/example/trackeroftherings/LoginUtility.java
@@ -24,4 +24,23 @@ public class LoginUtility {
         }
         return result;
     }
+
+    /**
+     * Finds the correct Company object for the input credentials and returns it
+     * If no such object exists returns null
+     * @param username
+     * @param password
+     * @param companyID
+     * @return Company object that credentials are correct for
+     */
+    public static Company companyLogin(String username, String password, String companyID){
+        ArrayList<Company> companies = DatabaseUtility.readCompanies(companyID);//Ideally will return a single Company
+        Company result = null;
+        for(Company c :companies){
+            if (c.getPassword().equals(password) && c.getUsername().equals(username)){
+                result = c;
+            }
+        }
+        return result;
+    }
 }
