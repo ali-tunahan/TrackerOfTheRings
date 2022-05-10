b4aba503d2e6411accd29343652fdb93fd097d9b
Perit Dinçer <peritdincer@gmail.com>
2022-05-10 19:59:01 +0300

    comment added

--- app/src/main/java/com/example/trackeroftherings/OnLocationUpdateListener.java
+++ app/src/main/java/com/example/trackeroftherings/OnLocationUpdateListener.java
@@ -1,7 +1,7 @@
 package com.example.trackeroftherings;
 
 import android.location.Location;
-
+//TAKEN FROM https://stackoverflow.com/questions/36436686/how-to-get-current-location-using-seperate-class-in-android
 public interface OnLocationUpdateListener {
     void onLocationChange(LocationPlus location);
     void onError(String error);
